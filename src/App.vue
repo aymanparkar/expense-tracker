2d0bd595ac8f3295071b6d34a5589f69616384e7
Ayman Parkar <ayman.parkar@gmail.com>
2024-01-27 20:04:51 +0530

    Add transaction form and table component to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,45 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <!-- outer most box -->
+  <div class="w-screen h-screen flex justify-center items-center">
+    <!-- main card -->
+    <div class="w-[700px] rounded-md border border-solid border-black p-4 space-y-4">
+      <!-- balance card -->
+      <div class="grow rounded-md border border-solid border-black divide-y divide-black">
+        <div class="grow p-4">Balance</div>
+        <div class="grow flex divide-x divide-black">
+          <div class="grow p-4">Income</div>
+          <div class="grow p-4">Expenses</div>
+        </div>
+      </div>
+      <!-- inputs -->
+      <div class="flex space-x-4">
+        <input
+          type="text"
+          name="Description"
+          id="email"
+          class="grow rounded-md border-0 py-1.5 text-gray-900 p-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          placeholder="Description"
+        />
+        <input
+          type="text"
+          name="Amount"
+          id="email"
+          class="grow rounded-md border-0 py-1.5 text-gray-900 p-2 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          placeholder="Amount"
+        />
+        <button
+          type="button"
+          class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Add Transaction
+        </button>
+      </div>
+      <!-- Table import table component-->
+      <Table />
+    </div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import Table from './components/Table.vue'
+</script>
---
+++ src/components/Table.vue
@@ -0,0 +1,48 @@
+<template>
+  <table class="min-w-full divide-y divide-gray-300">
+    <thead>
+      <tr>
+        <th
+          scope="col"
+          class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0"
+        >
+          Description
+        </th>
+        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+          Amount
+        </th>
+        <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-0">
+          <span class="sr-only">Edit</span>
+        </th>
+      </tr>
+    </thead>
+    <tbody class="divide-y divide-gray-200">
+      <tr v-for="(transaction, index) in transactions" :key="index">
+        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+          {{ transaction.description }}
+        </td>
+        <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+          {{ transaction.amount }}
+        </td>
+        <td
+          class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0"
+        >
+          <a href="#" class="text-indigo-600 hover:text-indigo-900"
+            >Delete<span class="sr-only"></span
+          ></a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const transactions = ref([
+  {
+    description: 'Coffee',
+    amount: 100
+  }
+])
+</script>
